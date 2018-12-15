21ef10eb6720a3ec6dcd510671fd20907f1e2734
peaceiris <30958501+peaceiris@users.noreply.github.com>
2018-12-16 00:36:34 +0900

    add Accessing the ClientContext

--- main.go
+++ main.go
@@ -1,16 +1,35 @@
 package main
 
 import (
+	"context"
+
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 )
 
-var msg = "Hello Gopher"
+// var msg = "Hello Gopher"
 
-func handler(request events.APIGatewayProxyRequest) (*events.APIGatewayProxyResponse, error) {
+// func handler(request events.APIGatewayProxyRequest) (*events.APIGatewayProxyResponse, error) {
+// 	return &events.APIGatewayProxyResponse{
+// 		StatusCode: 200,
+// 		Body:       msg,
+// 	}, nil
+// }
+
+func handler(ctx context.Context, request events.APIGatewayProxyRequest) (*events.APIGatewayProxyResponse, error) {
+	lc, ok := lambdacontext.FromContext(ctx)
+	if !ok {
+		return &events.APIGatewayProxyResponse{
+			StatusCode: 503,
+			Body:       "Something went wrong :(",
+		}, nil
+	}
+
+	cc := lc.ClientContext
+
 	return &events.APIGatewayProxyResponse{
 		StatusCode: 200,
-		Body:       msg,
+		Body:       "Hello, " + cc.Client.AppTitle,
 	}, nil
 }
 
