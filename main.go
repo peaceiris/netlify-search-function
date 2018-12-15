b2071f32bfbf8beb91011a51c6a29b7ded8b045d
peaceiris <30958501+peaceiris@users.noreply.github.com>
2018-12-16 00:16:33 +0900

    add sample

--- main.go
+++ main.go
@@ -1,28 +1,23 @@
 package main
 
 import (
-	"context"
 	"fmt"
 
 	"github.com/aws/aws-lambda-go/lambda"
 )
 
 type MyEvent struct {
-	Name string `json:"name"`
+	Name string `json:"What is your name?"`
 }
 
-// func handler(request events.APIGatewayProxyRequest, name MyEvent) (events.APIGatewayProxyResponse, error) {
-// 	return events.APIGatewayProxyResponse{
-// 		StatusCode: 200,
-// 		Body:       "My name is ",
-// 	}, nil
-// }
+type MyResponse struct {
+	Message string `json:"Answer:"`
+}
 
-func HandleRequest(ctx context.Context, name MyEvent) (string, error) {
-	return fmt.Sprintf("Hello %s!", name.Name), nil
+func hello(event MyEvent) (MyResponse, error) {
+	return MyResponse{Message: fmt.Sprintf("Hello %s!!", event.Name)}, nil
 }
 
 func main() {
-	// Make the handler available for Remote Procedure Call by AWS Lambda
-	lambda.Start(HandleRequest)
+	lambda.Start(hello)
 }
