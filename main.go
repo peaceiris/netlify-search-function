64f75410e27b65801ce4ea14267fc145deed5501
peaceiris <30958501+peaceiris@users.noreply.github.com>
2018-12-16 01:16:49 +0900

    restore

--- main.go
+++ main.go
@@ -1,35 +1,16 @@
 package main
 
 import (
-	"context"
-
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 )
 
-// var msg = "Hello Gopher"
+var msg = "Hello Gopher"
 
-// func handler(request events.APIGatewayProxyRequest) (*events.APIGatewayProxyResponse, error) {
-// 	return &events.APIGatewayProxyResponse{
-// 		StatusCode: 200,
-// 		Body:       msg,
-// 	}, nil
-// }
-
-func handler(ctx context.Context, request events.APIGatewayProxyRequest) (*events.APIGatewayProxyResponse, error) {
-	lc, ok := lambdacontext.FromContext(ctx)
-	if !ok {
-		return &events.APIGatewayProxyResponse{
-			StatusCode: 503,
-			Body:       "Something went wrong :(",
-		}, nil
-	}
-
-	cc := lc.ClientContext
-
+func handler(request events.APIGatewayProxyRequest) (*events.APIGatewayProxyResponse, error) {
 	return &events.APIGatewayProxyResponse{
 		StatusCode: 200,
-		Body:       "Hello, " + cc.Client.AppTitle,
+		Body:       msg,
 	}, nil
 }
 
